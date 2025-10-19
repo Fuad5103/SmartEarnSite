0ce19b21eb483c96bf71e38ebaafba880176a835
Fuad5103 <fisayev1977@gmail.com>
2025-10-19 18:32:25 +0400

    Initialize React application with SmartEarnSite component

---
+++ İndex..js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import SmartEarnSite from './SmartEarnSite';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <SmartEarnSite />
+  </React.StrictMode>
+);
